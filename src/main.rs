d13542691d57448a1e04846b0eb6f4391b1fb1e4
Will-Banksy <williamjb555@gmail.com>
2023-09-27 10:37:53 +0100

    Now makes 2 LEDs blink alternatingly

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,53 @@ use std::{process::ExitCode, io::{stdin, stdout, Write}, thread, time::Duration,
 
 use rust_gpiozero::OutputDevice;
 
+// trait WriteState {
+// 	fn write_value(&mut self, value: bool);
+// }
+
+// impl WriteState for OutputDevice {
+// 	fn write_value(&mut self, value: bool) {
+// 		if value {
+// 			self.on();
+// 		} else {
+// 			self.off();
+// 		}
+// 	}
+// }
+
 enum Instruction {
 	Terminate,
 }
 
 fn main() -> ExitCode {
-    // let mut led = LED::new(23);
+	print!("Enter blink delay (ms): ");
+	stdout().flush().unwrap();
+	let mut buf = String::new();
+	stdin().read_line(&mut buf).unwrap();
+	let buf = buf.trim();
+	let delay: u32 = buf.parse().unwrap_or(1000);
 
-    // led.blink(1.0, 1.0);
+	println!("Delay: {} ms", delay);
 
-    // print!("Press enter to continue...");
-    // stdout().flush().unwrap();
-    // let mut buf = String::new();
-    // stdin().read_line(&mut buf).unwrap();
+    let mut led1 = OutputDevice::new(23);
+	let mut led2 = OutputDevice::new(25);
 
-    // led.off();
-
-    let mut led = OutputDevice::new(23);
+	led1.on();
 
 	let (sender, reciever) = mpsc::channel::<Instruction>();
 
     let thandle = thread::spawn(move || {
 		loop {
-			led.on();
-			thread::sleep(Duration::from_secs(1));
-			
-			if let Ok(_instruct) = reciever.try_recv() {
-				break;
-			}
-			
-			led.off();
-			thread::sleep(Duration::from_secs(1));
+			led1.toggle();
+			led2.toggle();
+			thread::sleep(Duration::from_millis(delay as u64));
 			
 			if let Ok(_instruct) = reciever.try_recv() {
 				break;
 			}
 		}
-		led.off();
+		led1.off();
+		led2.off();
 	});
 
 	print!("Press enter to continue...");
